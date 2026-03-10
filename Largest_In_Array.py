3b3c885d449c52f6492dc5fb7b15c580f253e1e3
Rana Nirmal <nirmalrana108@gmail.com>
2026-03-10 20:10:27 +0530

    Update and rename README.md to Largest_In_Array.py

---
+++ Largest_In_Array.py
@@ -0,0 +1,7 @@
+array = [10 , 20 , 30 , 40 , 50 , 60 , 110 , 70 , 80 , 90 , 100 ]
+largest = array [0]
+
+for i in array :
+  if i > largest:
+    largest = i 
+print ("Largest in array is : " , largest)
